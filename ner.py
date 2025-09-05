960f49c6b6fbb1e7aa8c58af827d077a9c801c2a
pasesposito <pasesposito@unisa.it>
2025-09-05 15:56:09 +0200

    en_core_web_sm

--- ner.py
+++ ner.py
@@ -7,6 +7,13 @@ from pyvis.network import Network
 import tempfile
 import os
 
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 # -----------------------
 # Caricamento modelli
 # -----------------------
